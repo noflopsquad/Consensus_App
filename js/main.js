17bf337c17252bef2831e6054f856f5a324be97c
trikitrok <trikitrok@gmail.com>
2015-06-25 23:38:57 +0200

    Injecting a local storage persister to FirstTime page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 CUORE.Dom.ready(function() {
   var currentLocale = (navigator.language || navigator.browserLanguage);
-  
+
   document.labels = {};
   document.labels[currentLocale] = {
-      "a.key": "a literal",
+    "a.key": "a literal",
   };
-  
+
   FIRST_NAMES = [
-    "Big","Long", "Short","Small","Slow","Fast"
+    "Big", "Long", "Short", "Small", "Slow", "Fast"
   ];
 
   MIDDLE_NAMES = [
-    "Horny","Stupid", "Red","Blue","Yellow","Awful"
+    "Horny", "Stupid", "Red", "Blue", "Yellow", "Awful"
   ];
 
   LAST_NAMES = [
-    "Panda","Fox","Parrot","Armadillo","Babirusa","Condor","Porcupine","Peacock"
+    "Panda", "Fox", "Parrot", "Armadillo", "Babirusa", "Condor", "Porcupine", "Peacock"
   ];
-  
+
   CUORE.Bus.enableDebug();
 
-  document.page = new FirstTime();
+  document.page = new FirstTime(CUORE.LocalStorageStatePersister());
 
   document.page.draw();
 });
